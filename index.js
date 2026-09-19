813fe55f41cbc59102febc49fbef80eb32812208
yogurdegamba <212264429+yogurdegamba@users.noreply.github.com>
2026-09-19 15:59:15 +0200

    Coger_ip para ripe atlas

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ import express from "express"
 import cors from "cors"
 import { execFile } from "child_process"
 import { promisify } from "util"
-const execFileAsync = promisify(execFile) //para poder poner await y esperar los datos json que da la api sin que se ralle la funcion callback de execfile
+//const execFileAsync = promisify(execFile) //para poder poner await y esperar los datos json que da la api sin que se ralle la funcion callback de execfile
 const app = express()
+app.set("trust proxy", true)  //Para que de la ip del visitante y no el proxy
 app.use(cors())
 
 app.get("/api/traceroute", (req, res) => {
   const host = req.query.host;
+  const ipVisitante = req.ip
   if (!host || !(/^[a-zA-Z0-9.-]+$/.test(host || ""))){
     res.status(400).send("Solicitud inválida por error de sintaxis")
   }else{
-    Exec(host,res);
+    Exec(res,ipVisitante);
   }
 })
 
@@ -20,33 +22,10 @@ app.listen(3001, () => {
 })
 
 
-const Exec = (host, res) => {
-    execFile("traceroute", [host], async (error, stdout, stderr) => {
-    if (error) {
-        console.log("ERROR:", error)
-        console.log("STDERR:", stderr)
-        res.status(500).send(stderr)
-    }else{
-        const lineas = stdout.split("\n")
-        console.log(lineas)
-        const ips =[]
-        let ips_validas =[]
-        for (let i=0; i<lineas.length; i++){
-            ips[i] = lineas[i].match(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/)
-        }
-        ips_validas = ips.filter(element => element!==null) //function(element) {return element!== null}
-        console.log(ips_validas)
-        console.log(ips_validas[1][0])
-        const resultados = []
-        for (let i=0; i<ips_validas.length; i++){
-            const mapeo = await fetch(`http://ip-api.com/json/${ips_validas[i][0]}`)
-            const datos = await mapeo.json()
-            resultados.push(datos)
-        }
-        const resultadosValidos = resultados.filter(resultado => resultado.status === "success")
-        res.json(resultadosValidos)
-        console.log(resultadosValidos)
-    }
-    })
+const Exec = async(res,ip) => {
+    console.log(ip)
+    const mapeo = await fetch(`http://ip-api.com/json/${ip}`)
+    const ip_coordenadas = await mapeo.json()
+    console.log(ip_coordenadas)
 }
 
