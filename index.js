dafcca955b4c930fe25c3990a0cf155a19d21219
yogurdegamba <212264429+yogurdegamba@users.noreply.github.com>
2026-09-21 23:30:45 +0200

    Arreglar error 500

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.set("trust proxy", true)  //Para que de la ip del visitante y no el proxy
 app.use(cors())
 
 app.get("/api/traceroute", (req, res) => {
-    const ipVisitante = query.ip;
+    const ipVisitante = req.ip;
   const host = req.query.host;
   if (!host || !(/^[a-zA-Z0-9.-]+$/.test(host || ""))){
     res.status(400).send("Solicitud inválida por error de sintaxis")
@@ -40,13 +40,14 @@ const Exec = (host, res,ip) => {
         ips_validas = ips.filter(element => element!==null) //function(element) {return element!== null}
         console.log(ips_validas)
         console.log(ips_validas[1][0])
-        const resultados = [ip]
+        const resultados = []
         for (let i=0; i<ips_validas.length; i++){
             const mapeo = await fetch(`http://ip-api.com/json/${ips_validas[i][0]}`)
             const datos = await mapeo.json()
             resultados.push(datos)
         }
         const resultadosValidos = resultados.filter(resultado => resultado.status === "success")
+        resultadosValidos = [ip,resultadosValidos]
         res.json(resultadosValidos)
         console.log(resultadosValidos)
     }
